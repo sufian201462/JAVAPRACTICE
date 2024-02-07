a6563ea77ded41851e8c24701c3276ed7ffe737e
sufian201462 <38572598+sufian201462@users.noreply.github.com>
2024-02-07 13:48:54 +0600

    second java file fixed

--- second.java
+++ second.java
@@ -1,3 +1,5 @@
 public class second {
-    System.out.println("this is second java.i am using this for the second commit");
+    public static void main(String[] args){
+        System.out.println("this is second java.i am using this for the second commit");
+    }
 }
